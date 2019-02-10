523a75ff77098c5468a3f0923895f34ecb254f1e
Justin Sherman <jsherman@MacBook-Pro-2.local>
2019-02-10 04:52:50 -0800

    Added a bunch of comments

--- sudoku.py
+++ sudoku.py
@@ -4,6 +4,7 @@ minigrid = 3
 import math
 
 class Square:
+    # A set<int> that stores the value(s) of the Square
     values = set()
 
     # __init__      initialize Square object
@@ -44,29 +45,30 @@ class Square:
         return self.isSolved()
 
 
-    #def __str__(self):
-    #    if self.isSolved():
-    #        return str(list(self.values)[0])
-        #elif self.isUnsolved():
-        #    return "?" # If completely unsolved, shorten to '?'
-    #    else:
-    #        return str(self.values)
-
-    #def __repr__(self):
-    #    return self.__str__()
-
+    # html          Get info used for rendering Square as HTML
+    #
+    # Returns:      tuple(CSS class to use for display, value(s) to display)
+    #           
     def html(self):
         if self.isSolved():
-            return ("solved", str(list(self.values)[0]))
+            return ('solved', str(list(self.values)[0]))
         elif self.isUnsolved():
-            return ("","")
+            return ('','')
         else:
             return ('hints', ' '.join(str(x) for x in self.values))
 
 
+
 class Grid:
+    # A 2D list of Square objects representing the sudoku grid
     _grid = None
 
+
+    # __init__      Construct an empty grid or load a puzzle from a file
+    #
+    # Inputs:
+    #       puzzle  Path to the text file of a puzzle to load
+    #
     def __init__(self, puzzle = None):
         if puzzle:
             f = open(puzzle)
@@ -74,12 +76,9 @@ class Grid:
         else:
             self._grid = [[Square(n) for i in range(0,n)] for j in range(0,n)]
 
-    #def __str__(self):
-    #    return str(self._grid)
 
-    #def __repr__(self):
-    #    return self.__str__()
-
+    # html          Generate HTML representation of the sudoku grid
+    #
     def html(self):
         h = '<link rel="stylesheet" type="text/css" href="/static/style.css">'
         h += "<table>\n"
@@ -107,27 +106,76 @@ class Grid:
         h += "</table>"
         return h
 
+
+    # get           Get a reference to a Square object in the Grid
+    #
+    # Inputs:
+    #           i   Row index
+    #           j   Column index
+    #
+    # Returns:      reference to the requested Square object
+    #
     def get(self, i, j):
         return self._grid[i][j]
 
+
+    # reduce        Convenience function to reduce a specific Square object
+    #
+    # Inputs:
+    #           i   Row index
+    #           j   Column index
+    #           s   set<int> of values to rule out from the Square
+    #
+    # Returns:      True if the Square is now solved
+    #
     def reduce(self, i, j, s):
         return self._grid[i][j].reduce(s)
 
-    # Given i,j coords, return all squares in that same row
+
+    # getRow        Get all Square objects in the same row as a given Square
+    #
+    # Inputs:
+    #           i   Row index
+    #           j   Column index
+    #
+    # Returns:      All Squares in the same row as (i,j) as a list
+    # 
     def getRow(self, i, j):
         return self._grid[i]
 
-    # Given i,j coords, return all squares in that same column
+
+    # getCol        Get all Square objects in the same column as a given Square
+    #
+    # Inputs:
+    #           i   Row index
+    #           j   Column index
+    #
+    # Returns:      All Squares in the same column as (i,j) as a list
+    #
     def getCol(self, i, j):
         return [x[j] for x in self._grid]
 
-    #TODO make this independent of the puzzle size
+
+    # getSub        Get all Square objects in the same subgrid as a given Square
+    #
+    # Inputs:
+    #           i   Row index
+    #           j   Column index
+    #
+    # Returns:      All Squares in the same subgrid as (i,j) as a flattened list
+    #
+    # TODO make this independent of the puzzle size
     def getSub(self, i, j):
         i = math.floor(i/3)
         j = math.floor(j/3)
         l = [x[j*3:(j+1)*3] for x in self._grid[i*3:(i+1)*3]]
         return [i for x in l for i in x] # Flatten the list
 
+
+    # isSolved      Find out if the entire puzzle is solved
+    #
+    # Returns:      True if the puzzle is solved. False otherwise
+    #
     def isSolved(self):
         for i in range(0, n):
             for j in range(0, n):
